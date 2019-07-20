c781b9e11407bd576d84e5fbe557e3d8aabfd452
codepriezt <kolumide22@gmail.com>
2019-07-19 20:38:24 -0400

    voting

---
+++ vote.php
@@ -0,0 +1,20 @@
+<?php
+
+include('database.php');
+
+
+if(isset($_POST["vote_option"]))
+{
+    $query = "
+    INSERT into tbl_vote
+    (vote_candidates) VALUES (:vote_candidates)
+    ";
+
+    $data = array(
+        ':vote_candidates' => $_POST["vote_option"]
+    );
+    $statement = $connect->prepare($query);
+    $statement->execute($data);
+}
+
+?>
